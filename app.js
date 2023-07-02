74b1242a40581677cfbfe67140493f7e73f14fb3
rafaelranery <rafaelrapnery@gmail.com>
2023-07-01 22:10:32 -0300

    deploy ready

--- app.js
+++ app.js
@@ -72,5 +72,5 @@ mongoose
   .connect(
     `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.sp4cm6j.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
   )
-  .then(() => app.listen(5000)) // if success start server
+  .then(() => app.listen(process.env.PORT || 5000)) // if success start server
   .catch((err) => console.log(err)); // else throw erro
